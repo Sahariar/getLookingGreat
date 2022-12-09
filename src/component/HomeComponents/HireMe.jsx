a8723c3488005534896e61e5ae4aa36f24a76369
Sahariar Kabir <sronok@gmail.com>
2022-12-09 23:09:53 +0600

    fix hire me section

--- src/component/HomeComponents/HireMe.jsx
+++ src/component/HomeComponents/HireMe.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HireMe = () => {
     return (
         <section className="my-10">
-	<div className="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
+	<div className="container xs:max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-4">
 		<div>
 			<h2 className="text-3xl font-bold tracking-tight text-center sm:text-5xl ">Why Hire Me ?</h2>
 			<p className="max-w-3xl mx-auto mt-4 text-xl text-center ">Benefits to my Clients.</p>
